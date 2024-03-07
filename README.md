e8f2e149edcdc18bd503a8d45a549a2f326ce848
MG <ghaddarmoustafa@gmail.com>
2024-03-07 16:30:34 +0200

    Add moving logic for the Knight

--- README.md
+++ README.md
@@ -1,13 +1,45 @@
 ## Intro
 
-THis is a quick test of the chess logic not an actual imlementation:
+This is a quick test of the chess logic not an actual imlementation:
 
-Given the positions of a white bishop and a black pawn on the standard chess board, determine whether the bishop can capture the pawn in one move.
+1. 
+	Given the positions of a white bishop and a black pawn on the standard chess board, determine whether the bishop can capture the pawn in one move.
 
-The bishop has no restrictions in distance for each move, but is limited to diagonal movement.
+	The bishop has no restrictions in distance for each move, but is limited to diagonal movement.
+
+2. 
+	Given a position of a knight on the standard chessboard, find the number of different moves the knight can perform.
+
+	The knight can move to a square that is two squares horizontally and one square vertically, or two squares vertically and one square horizontally away from it. The complete move therefore looks like the letter L. Check out the image below to see all valid moves for a knight piece that is placed on one of the central squares
 
 ## Example
 
-For bishop = "a1" and pawn = "c3", the output should be solution(bishop, pawn) = true.
+1.
+	For bishop = "a1" and pawn = "c3", the output should be solution(bishop, pawn) = true.
 
-For bishop = "h1" and pawn = "h3", the output should be solution(bishop, pawn) = false.
+	For bishop = "h1" and pawn = "h3", the output should be solution(bishop, pawn) = false.
+
+2.
+	For cell = "a1", the output should be solution(cell) = 2.
+
+	For cell = "c2", the output should be solution(cell) = 6.
+
+## Chess Board
+
+8: A B C D E F G H
+
+7: A B C D E F G H
+
+6: A B C D E F G H
+
+5: A B C D E F G H
+
+4: A B C D E F G H
+
+3: A B C D E F G H
+
+2: A B C D E F G H
+
+1: A B C D E F G H
+
+    
--- src/chess.c
+++ src/chess.c
@@ -2,6 +2,27 @@
 #include <string.h>
 #include <stdio.h>
 
+#define NUM_FUNC 8
+
+//#define DEBUG 1
+#ifdef DEBUG
+#define ENTER(x, y)    do {\
+                            printf("%s:ENTER(%s)\n", x, y);\
+                       } while(0)
+#define EXIT(x, y)    do {\
+                            printf(x);\
+                            printf(":EXIT(%d)\n", y);\
+                         } while(0)
+#else
+
+#define ENTER(x, y)
+#define EXIT(x, y)
+
+#endif
+
+/*************************************/
+/*            BISHOP                 */
+/*************************************/
 bool direction_1(char * bishop, char * pawn)
 {
     //printf("%s: ENTER()\n", __func__);
@@ -136,7 +157,7 @@ bool direction_4(char * bishop, char * pawn)
     return ret;
 }
 
-bool solution(char * bishop, char * pawn)
+bool solution_bishop(char * bishop, char * pawn)
 { 
     bool ret = false;
 
@@ -166,6 +187,105 @@ out:
     return ret;
 }
 
+/*************************************/
+/*            KNIGHT                 */
+/*************************************/
+
+bool is_valid_cell(char * s)
+{
+    bool ret = true;
+    int size = strlen(s);
+    ENTER(__func__, s);
+    
+    if (!size || !s)
+        goto out;
+        
+    if (!(s[1] >= 0x31 && s[1] <= 0x38 && s[0] >= 0x61 && s[0] <= 0x68))
+        ret = false;
+ out:
+    EXIT(__func__, ret);   
+    return ret;
+}
+
+void direction_1_k(char * temp)
+{
+    temp[0] += 1;
+    temp[1] += 2;
+}
+void direction_2_k(char * temp)
+{
+    temp[0] += 2;
+    temp[1] += 1;
+}
+void direction_3_k(char * temp)
+{
+    temp[0] += 1;
+    temp[1] -= 2;
+}
+void direction_4_k(char * temp)
+{
+    temp[0] += 2;
+    temp[1] -= 1;
+}
+
+void direction_1_opposite(char * temp)
+{
+    temp[0] -= 1;
+    temp[1] += 2;
+}
+void direction_2_opposite(char * temp)
+{
+    temp[0] -= 2;
+    temp[1] += 1;
+}
+void direction_3_opposite(char * temp)
+{
+    temp[0] -= 1;
+    temp[1] -= 2;
+}
+void direction_4_opposite(char * temp)
+{
+    temp[0] -= 2;
+    temp[1] -= 1;
+}
+
+
+int solution_knight(char * cell) 
+{
+    int ret = 0;
+    void (*f_ptr[NUM_FUNC])(char * c);
+    char temp[3] = { 0 };
+    
+    if (!cell || !strlen(cell))
+        goto out;
+    
+    ENTER(__func__, cell);
+    
+    f_ptr[0] = direction_1_k;
+    f_ptr[1] = direction_2_k;
+    f_ptr[2] = direction_3_k;
+    f_ptr[3] = direction_4_k;
+    
+    f_ptr[4] = direction_1_opposite;
+    f_ptr[5] = direction_2_opposite;
+    f_ptr[6] = direction_3_opposite;
+    f_ptr[7] = direction_4_opposite;      
+           
+    for (int i = 0; i < NUM_FUNC; ++i) {
+        
+        memcpy(temp, cell, strlen(cell));
+        f_ptr[i](temp);
+        ret += is_valid_cell(temp);
+    }
+    
+out:
+    EXIT(__func__, ret);
+    return ret;
+}
+
+/*************************************/
+/*              MAIN                 */
+/*************************************/
 int main(void)
 {
     int ret = 0;
@@ -176,9 +296,19 @@ int main(void)
 
     for (int i = 0; i < 22; i += 2) {
 
-        ret = solution(input[i], input[i + 1]);
-        printf("Pawn @ %s will be%scaptured by the Bishop @ %s\n", input[i + 1], ret ? " ": " NOT ", input[i]);
-            
+        ret = solution_bishop(input[i], input[i + 1]);
+        printf("Pawn @ %s will be%scaptured by the Bishop @ %s\n", input[i + 1], ret ? " ": " NOT ", input[i]);   
+    }
+
+    char input_knight[10][3] = {"a1", "c2", "d4", "g6", "a3", "b7", "h8", "h6", "g8", "a5"};
+    int expected_output_knight[10] = {2, 6, 8, 6, 4, 4, 2, 4, 3, 4};
+
+    for (int i = 0; i < 10; ++i) {
+        printf("Test #%d: ", i + 1);
+        if (expected_output_knight[i] != solution_knight(input_knight[i]))
+            printf("FAIL\n");
+        else
+            printf("PASS\n");
     }
 
     printf("GDBYE CHESS WORLD\n");
